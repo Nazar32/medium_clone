c6d233fb03f3ed5c9e523a0dcd9a7056ec5c9071
trudyga <trudyga0822@gmail.com>
2018-06-07 23:19:25 +0300

    Initial React Project Commit
    1. scss support was added.

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './styles/App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div>App works</div>
+    );
+  }
+}
+
+export default App;
